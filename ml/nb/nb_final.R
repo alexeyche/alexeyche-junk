530999292b022b6b7d327f6406eada162907b8b6
alexeyche <alexey.chernushev@gmail.com>
2012-11-07 20:58:55 +0400

    nb

--- ml/nb/nb_final.R
+++ ml/nb/nb_final.R
@@ -12,12 +12,12 @@ x.test <- test[1:4]
 n <- ncol(x.d)
 m <- nrow(x.d)
 
-#normalize
-for(i in 1:n) {
-    x.d[,i] <- (x.d[,i] - mean(x.d[,i]))/sd(x.d[,i])
-    test[,i] <- (test[,i] - mean(test[,i]))/sd(test[,i])
-}
+source("../featureCook.R")
 
+x.d <- featureNormalize(x.d)
+test[,1:4] <- featureNormalize(test[,1:4])
+
+ 
 # vector form
 K <- function(r) {
   ignore <- abs(r) > 1
@@ -26,7 +26,7 @@ K <- function(r) {
   return(r)
 }
 
-# x     - x1  x2  .. xN
+# x     - x1  x2  .. xN   - vector for point
 # x.all - x11 x12 .. x1N  - feature matrix
 #         x21 x22 .. x2N
 #         ...
@@ -44,24 +44,29 @@ parzen_window <- function(x, x.all, h) {
   sum(kernels_pr)/m
 }
 
-eval_parzen_window <- function(x.all, h) {
-    p <- NULL
-    for(i in 1:nrow(x.all)) {
-        x.i <- matrix(x.all[i,], nrow=1)
-        p <- rbind(p, parzen_window(x.i, x.all, h))
-    }
-    return(p)
-}
+
 
 test_parzen_window <- function() {
+    
+    eval_parzen_window <- function(x.all, h) {
+        p <- NULL
+        for(i in 1:nrow(x.all)) {
+            x.i <- matrix(x.all[i,], nrow=1)
+            p <- rbind(p, parzen_window(x.i, x.all, h))
+        }
+        return(p)
+    }    
+    
     library(scatterplot3d)
     x.t <- as.matrix( cbind(rnorm(2000),rnorm(2000)) )
     par(mfrow=c(3,3))
     for(i in 1:9) {
-       p <- eval_parzen_window(x.t, 0.3)
+        p <- eval_parzen_window(x.t, i*0.1)
         scatterplot3d(x.t[,1],x.t[,2], p)
     }
 }
+
+
 test_nb <- function() {
     for(i in 1:nrow(test)) {
         h <- 0.4
@@ -73,12 +78,6 @@ test_nb <- function() {
             p <- parzen_window(test_vector,x.d_y, h)
             cat(class," in ", p*py, " is ",goal, "\n")
         }
-        cat("//------------------\n")
+        cat("//------------------------------------------------------------------------\n")
     }
 }
-
-#    
-#
-#    cat("versicolor in ", p2*py, " is ", goal, "\n")
-#    cat("virginica in ", p3*py, " is ", goal, "\n")
-#}
