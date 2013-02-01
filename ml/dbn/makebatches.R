86a26abeefb5e373447f02bfe2363260880913e2
alexeyche <alexey.chernushev@gmail.com>
2013-02-01 21:41:09 +0400

    dbn

--- ml/dbn/makebatches.R
+++ ml/dbn/makebatches.R
@@ -7,18 +7,32 @@ num_digits <- 10
 batchsize <- 100
 if( ! file.exists(".RData")) {
     digitdata <- NULL
+    digitdata.t <- NULL
     targets <- NULL
+    targets.t <- NULL
 
     empty_target <- rep(0, num_digits)
     
     for(i in 0:9) {
         filename <- sprintf("digit%d.mat",i)
+        filename.t <- sprintf("test%d.mat",i) # .t means test data
+
         d <- readMat(filename)
+        d.t <- readMat(filename.t)
+        
         m <- nrow(d$D)
+        m.t <- nrow(d.t$D)
+        
         digitdata <- rbind(digitdata, d$D)
+        digitdata.t <- rbind(digitdata.t, d.t$D)
+        
         target <- empty_target
+        target.t <- empty_target
+        
         target[i+1] <- 1   # 1 0 0 0 0 0 0 0 0 0 for first case
+        target.t[i+1] <- 1   # 1 0 0 0 0 0 0 0 0 0 for first case
         targets <- rbind(targets, matrix(rep(target,m), ncol=num_digits, byrow=TRUE))
+        targets.t <- rbind(targets, matrix(rep(target,m), ncol=num_digits, byrow=TRUE))
     }
     digitdata <- digitdata/255  # normalize
     
