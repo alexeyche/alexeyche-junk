70f61670bf38381a01d59a5f7e71ec2da24ffb45
alexeyche <alexey.chernushev@gmail.com>
2013-02-08 19:54:00 +0400

    dbn

--- ml/dbn/iris_classify.R
+++ ml/dbn/iris_classify.R
@@ -8,6 +8,7 @@ maxepoch <- 50
 num.hid <- 8; numpen <- 500; numpen2 <- 2000
 
 data(iris)
+num.classes <- 3
 
 c(batchdata,batchtargets) := makebatches( iris[,1:4], as.matrix(iris[,5]), 50)
 c(num.cases, num.dims, num.batches) := dim(batchdata)
@@ -19,3 +20,29 @@ train.params = list(e.w = 0.1, e.v = 0.1, e.h = 0.1, w_cost = 0.0002,
                     epochs = 50, cd.iter = 1)   
 
 c(rbm.model,batch.pos.hid.probs) := train_rbm(batchdata, train.params, num.hid)
+
+w1 <- rbind(rbm.model$W, rbm.model$hid_bias) 
+w_class <- 0.1*matrix(rnorm( (ncol(w1)+1)*num.classes ), nrow = (ncol(w1)+1), num.classes)
+
+l1 <- nrow(w1)-1
+l2 <- nrow(w_class)-1
+
+test_err <- NULL
+train_err <- NULL
+
+maxepoch <- 100
+
+#for(epoch in 1:maxepoch) {
+    err <-0
+    err_cr <- 0
+    counter <- 0
+    c(numcases numdims numbatches) := dim(batchdata)
+    N <- numcases
+#    for(batch in 1:numbatches) {
+        batch <-1
+        data <- batchdata[,,batch]
+        targetdata <- 
+
+
+#    }
+#}
