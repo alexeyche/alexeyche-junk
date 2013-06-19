27e8e61b428dd10e706d5b1fad7590618bef189d
alexeyche <alexey.chernushev@gmail.com>
2013-06-19 20:35:58 +0400

    cns

---
+++ cns/cpp/src/sim/socket/sim_socket_core.h
@@ -0,0 +1,31 @@
+#ifndef SIM_SOCKET_CORE_H
+#define SIM_SOCKET_CORE_H
+
+#include "sim_client.h"
+
+#include <string.h>
+
+#include <sim/core.h>
+
+#define HEAD_LEN 100
+#define PORT 7778
+
+void send_arma_mat(mat m, std::string name, long unsigned int* iter=NULL, bool verbose=false) {
+	double *p = m.memptr();
+	const char *name_c = name.c_str()	;
+	char head_m[HEAD_LEN];
+	if(iter == NULL) {
+		snprintf(head_m, 100, "%s:%d:%d@",name_c, m.n_rows, m.n_cols);
+	} else 	{
+		snprintf(head_m, 100, "%s%d:%d:%d@",name_c,(int)*iter, m.n_rows, m.n_cols);
+	}
+	
+	init_socket(PORT);	
+	send_message<char>(head_m, HEAD_LEN);	
+	send_message<double>(p, sizeof(double)*m.n_rows*m.n_cols);
+	if(verbose) {
+		std::cout << "sending: " << head_m << std::endl;
+	}		
+}
+
+#endif
