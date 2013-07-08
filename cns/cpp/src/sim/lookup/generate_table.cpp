2731faffd380e89723c11ba4b027cadd087bcb53
alexeyche <alexey.chernushev@gmail.com>
2013-07-08 20:27:25 +0400

    cns

--- cns/cpp/src/sim/lookup/generate_table.cpp
+++ cns/cpp/src/sim/lookup/generate_table.cpp
@@ -28,21 +28,26 @@ int main(int argc, char** argv)
 {
     CLI::ParseCommandLine(argc, argv);
     LookupTableIzh lut;
-    double Vs = -70.0;
+    double Vs = -105.0;
     double us = -14.0;
     double Is = 5;
     NeuronIzh n;
-    for(size_t icyc=0; icyc<10; icyc++) {
-        for(double t=0; t<50; t+=1) {
-            lut.getValue(Vs, us, Is, t);
-        }
-        double u = lut.getLastU(Vs, us, Is);
-        //std::cout << "last u: " << u << "\n";
-        double V = lut.getValue(Vs, us, Is, 49.0);
-        //std::cout << "last V: " << V << "\n";
-        Vs=V;
-        us=u;
-    }
+    Log::Info << lut.getIndex("V", Vs).low << " | " << lut.getIndex("V", Vs).high << std::endl;
+    Log::Info << lut.getIndex("u", us).low << " | " << lut.getIndex("u", us).high << std::endl;
+    Log::Info << lut.getIndex("I", Is).low << " | " << lut.getIndex("I", Is).high << std::endl;
+    Log::Info << lut.getValue(lut.getIndex("V", Vs).low, lut.getIndex("u", us).low, lut.getIndex("I", us).low, 0) << " - ";
+    Log::Info << lut.getValue(lut.getIndex("V", Vs).high, lut.getIndex("u", us).high, lut.getIndex("I", us).high, 0)  << std::endl;
+    // for(size_t icyc=0; icyc<50; icyc++) {
+    //     for(double t=0; t<50; t+=1) {
+    //         lut.getValue(Vs, us, Is, t);
+    //     }
+    //     double u = lut.getLastU(Vs, us, Is);
+    //     //std::cout << "last u: " << u << "\n";
+    //     double V = lut.getValue(Vs, us, Is, 49.0);
+    //     //std::cout << "last V: " << V << "\n";
+    //     Vs=V;
+    //     us=u;
+    // }
 
     return 0;
 }
--- cns/cpp/src/sim/lookup/lookup_table.h
+++ cns/cpp/src/sim/lookup/lookup_table.h
@@ -12,8 +12,10 @@ struct dim {
     double dx;
     size_t it_size;
     size_t it_size_no_t;
-    std::string name;
     
+    std::string name;
+    vec space;
+
     bool equals(const dim &other_dim) {
         if(dim_size != other_dim.dim_size) { return false; }
         if(min != other_dim.min) { return false; }
@@ -21,11 +23,12 @@ struct dim {
         if(dx != other_dim.dx) { return false; }
         if(it_size != other_dim.it_size) { return false; }
         if(it_size_no_t != other_dim.it_size_no_t) { return false; }
-        //if(name != other_dim.name) { return false; }
+        //if(name != other_dim.name) { return false; }        
 
         return true;
     }
 };
+
 struct pointIzh {
     pointIzh(size_t Vi,  size_t ui, size_t Ii, size_t ti = -1) : Vi(Vi), ui(ui), Ii(Ii), ti(ti) {}
     size_t Vi;
@@ -33,6 +36,7 @@ struct pointIzh {
     size_t Ii;
     size_t ti;
 };
+
 struct fileHead {
         unsigned int dims_num;
         dim *dims; 
@@ -70,6 +74,7 @@ public:
         d.max = max;
         d.dx = (max-min)/dim_size;
         d.name = name.c_str();
+        d.space = linspace<vec>(d.min, d.max, d.dim_size);
         dims[name] = d;       
         dims_size *= d.dim_size;
 
@@ -90,12 +95,11 @@ public:
             }   
         }
     }
-    vec getSpaceVec(const std::string axis_name) {
-        return linspace<vec>(dims[axis_name].min, dims[axis_name].max, dims[axis_name].dim_size);
+    vec& getSpaceVec(const std::string axis_name) {
+        return dims[axis_name].space;
     }
     void placeState(pointIzh p, state_type &state) {
         size_t Vi_it, ui_it, Ii_it, ti_it;
-        //Log::Info << p.Vi << ":" << p.ui << ":" << p.Ii << ":" << p.ti << " = " << state[0] << std::endl;
         Vi_it = p.Vi * dims["V"].it_size;
         ui_it = p.ui * dims["u"].it_size;
         Ii_it = p.Ii * dims["I"].it_size;
@@ -106,7 +110,6 @@ public:
             Vi_it = p.Vi * dims["V"].it_size_no_t;
             ui_it = p.ui * dims["u"].it_size_no_t;
             Ii_it = p.Ii * dims["I"].it_size_no_t;
-            //Log::Info << p.Vi << ":" << p.ui << ":" << p.Ii << " = " << state[1] << std::endl;
             u_fin[Vi_it + ui_it + Ii_it] = state[1]; // putting final u
         }            
     }
@@ -142,7 +145,10 @@ public:
 #define FILENAME "NeuronIzhData.bin"
 
 class LookupTableIzh {
-
+struct LinealInterpIndex {
+    int low;
+    int high;
+};
 public:
 	LookupTableIzh() : curr_time(0)  {		
         NeuronIzh n;
@@ -151,32 +157,41 @@ public:
 	}
 	~LookupTableIzh() { delete table; }
 	
-	float getValue(double V, double u, double I, double t) {
-		size_t vi = getIndex("V", V);
-		size_t ui = getIndex("u", u);
-		size_t ii = getIndex("I", I);
-        size_t ti = getIndex("t", t);
-		
+	float getValue(size_t vi, size_t ui, size_t ii, size_t ti) {
 		size_t vi_it = vi  * table->dims["V"].it_size;
 		size_t ui_it = ui * table->dims["u"].it_size;
         size_t ii_it = ii * table->dims["I"].it_size;
 
-		//std::cout << "vi: " << vi << "\t" << "ui: " << ui << "\t" << "ti: " << ti << "\n";
-		//std::cout << "vi_it: " << vi_it << "\t" << "ui_it: " << ui_it << "\n";
-		std::cout << table->v[vi_it+ui_it+ii_it+ti] << "\n";
 		return table->v[vi_it+ui_it+ii_it+ti];
-	}
+    }
+    float bilineal_interpolation(double V, double u, double I, double t) {
+        LinealInterpIndex vi = getIndex("V", V);
+        LinealInterpIndex ui = getIndex("u", u);
+        LinealInterpIndex ii = getIndex("I", I);
+        LinealInterpIndex ti = getIndex("t", t);
 
-	float getLastU(double V, double u, double I) {
-		size_t vi = getIndex("V", V);
-		size_t ui = getIndex("u", u);
-        size_t ii = getIndex("I", I);
+    }
+    LinealInterpIndex getIndex(const std::string &axis_name, double value) {       
+        LinealInterpIndex ind;
+        ind.low = floor((value - table->dims[axis_name].min)/table->dims[axis_name].dx);  // -100 -70
+        //if(ind.low < table->dims[axis_name].dim_size) {
+        ind.high = ind.low+1;
+        //} else {
+        //    ind.high = ind.low;
+        //}
+        return ind;
+    }   
 
-		size_t vi_it = vi * table->dims["u"].dim_size * table->dims["I"].dim_size;
-        size_t ui_it = ui * table->dims["I"].dim_size;
+	// float getLastU(double V, double u, double I) {
+	// 	size_t vi = getIndex("V", V);
+	// 	size_t ui = getIndex("u", u);
+ //        size_t ii = getIndex("I", I);
 
-		return table->u_fin[vi_it+ui_it+ii];
-	}
+	// 	size_t vi_it = vi * table->dims["u"].dim_size * table->dims["I"].dim_size;
+ //        size_t ui_it = ui * table->dims["I"].dim_size;
+
+	// 	return table->u_fin[vi_it+ui_it+ii];
+	// }
 
 	void generate_table(Model *m) {    	
         table = new TableIzh();
@@ -194,14 +209,12 @@ public:
            return;
         }
 
-//        return;
-        
         Timer::Start("Table generation");
         
-        vec V_space = table->getSpaceVec("V");
-        vec u_space = table->getSpaceVec("u");
-        vec I_space = table->getSpaceVec("I");
-        vec t_space = table->getSpaceVec("t");
+        vec& V_space = table->getSpaceVec("V");
+        vec& u_space = table->getSpaceVec("u");
+        vec& I_space = table->getSpaceVec("I");
+        vec& t_space = table->getSpaceVec("t");
 
         for(size_t vi=0; vi<V_space.n_elem; vi++) {
             for(size_t ui=0; ui<u_space.n_elem; ui++) {
@@ -223,43 +236,7 @@ public:
         Log::Info << "Table generated. Saving data to file " << filename << std::endl;
         saveToFile();                       
     }
-    void check_bounds(Model *m) {
-        float V0 = -70;
-        float u0 = -14;
-        float V_min=100; float V_max=-100; float u_min=100;  float u_max=-100;
 
-        vec V_space = linspace<vec>(-105,30.0,100);
-        vec u_space = linspace<vec>(-15.5, 36.5,100);
-        vec I_space = linspace<vec>(-70,70,100);
-        vec t_space = linspace<vec>(0,50,100);
-
-        for(size_t vi=0; vi<V_space.n_elem; vi++) {
-            for(size_t ui=0; ui<u_space.n_elem; ui++) {
-                for(size_t ii=0; ii<I_space.n_elem; ii++) {
-                    state_type x(3);
-                    x[0] = V_space(vi);
-                    x[1] = u_space(ui);
-                    x[2] = I_space(ii);
-                    for(size_t ti=0; ti<t_space.n_elem; ti++) {
-                            m->do_step(x, t_space(ti)); 
-                            if (V_min > x[0]) {
-                                V_min = x[0];
-                            }         
-                            if(V_max < x[0]) {
-                                V_max = x[0];
-                            }     
-                            if(u_min > x[1]) {
-                                u_min = x[1];
-                            }         
-                            if(u_max < x[1]) {
-                                u_max = x[1];
-                            }                                
-                    }
-                }
-            }
-        }   
-        std::cout << "V_min: " << V_min << " V_max: " << V_max << " u_min: " << u_min << " u_max: " << u_max << std::endl;        
-    }
     void saveToFile() {
          FILE *bin_data = fopen(FILENAME,"wb");
          fileHead * fh = table->generateFileHeader();
@@ -359,9 +336,7 @@ public:
     }
 
 private:
-	size_t getIndex(const std::string &axis_name, double value) {		
-		return (size_t)(value - table->dims[axis_name].min)/table->dims[axis_name].dx;  // -100 -70
-	}	
+
 	TableIzh *table;
 	double curr_time;
     const std::string filename;
