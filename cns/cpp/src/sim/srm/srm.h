978bd29e74e38d427d78b640fcaa982aeda3a751
alexeyche <alexey.chernushev@gmail.com>
2013-10-26 15:08:20 +0400

    cns

--- cns/cpp/src/sim/srm/srm.h
+++ cns/cpp/src/sim/srm/srm.h
@@ -1,13 +1,68 @@
 
 
 namespace srm {
-    const double ts = 0.7; // ms
+    const double ts = 3; // ms
     const double tm = 10; // ms
     
     double epsp(double t, double fj, double fi) {
-        if( ((t-fj)<0) || ((t-fi)<0) || (fj<0) ) return 0.0;
+        if( ((t-fj)<0) ||  (fj<0) ) return 0.0;
         return (exp( -std::max(fi-fj, 0.0)/ts )/(1-ts/tm)) * (exp(-std::min(t-fi,t-fj)/tm) - exp(-std::min(t-fi,t-fj)/ts));
     }
+
+
+
+    const double u_abs = -100; // mV
+    const double u_r = -50; // mV
+    const double trf = 0.25; // ms
+    const double trs = 3; // ms
+    const double dr = 1; // ms
+
+    double nu(double t, double fi) {
+        if((t-fi)<0) return 0;
+        if((t-fi)<dr) return u_abs;
+        return u_abs*exp(-(t-fi+dr)/trf)+u_r*exp(-(t-fi)/trs);
+    }
+
+    const double u_rest = -70; //mV
+    class SrmNeuron {
+
+    public:
+        typedef std::vector<SrmNeuron*> TInput;
+        SrmNeuron() {
+        }
+
+        double u(double t) {
+            double epsp_pot = 0;
+            double nu_pot = 0;
+            for(size_t i=0; i<in.size(); i++) {
+                Log::Info << "i:" << i << " ";
+                for(size_t j=0; j< in[i]->y.n_elem; j++) {
+                    if(y.n_elem>0) {
+                        epsp_pot += w(i)*srm::epsp(t, in[i]->y(j), y.max());
+                    } else {
+                        epsp_pot += w(i)*srm::epsp(t, in[i]->y(j), -datum::inf);
+                    }
+                    Log::Info << "epsp_pot: " << epsp_pot << "\n";
+                }
+            }
+            for(size_t i=0; i<y.n_elem; i++) {
+                nu_pot += srm::nu(t, y(i));
+            }
+            return u_rest + epsp_pot + nu_pot;
+        }
+
+        vec* get_y() {
+            return &y;
+        }
+        void add_input(SrmNeuron *n, double w_n) {
+           in.push_back(n);
+           w << w_n;
+        }
+        vec w;
+        vec y;
+        TInput in;
+    };
+
 };
 
 
--- cns/cpp/src/sim/srm/srm_test.cpp
+++ cns/cpp/src/sim/srm/srm_test.cpp
@@ -6,33 +6,95 @@
 #include "srm.h"
 
 
-void epsp_test() {
+void epsp_test(bool just_print = false) {
     double Tmax = 100;
     double dt = 0.1;
     vec t = linspace<vec>(0.0, Tmax, (int)Tmax/dt);
-    
-    double fi = 10.0;
-    double fj = 5.0; 
+
+    double fi = 0;
+    double fj = 10;
     mat epsp(t.n_elem, 2);
     for(size_t ti=0; ti<t.n_elem; ti++) {
-        epsp[ti, 0] = t[ti];
-        epsp[ti, 1] = srm::epsp(t[ti], fj, fi);
+        if(t[ti] >= 15) {
+            fi = 15; 
+        }
+        epsp(ti, 0) = t[ti];
+        epsp(ti, 1) = srm::epsp(t[ti], fj, fi);
     }
-    send_arma_mat(epsp, "epsp");
+    if(just_print) {
+        epsp.print();
+    } else {        
+        send_arma_mat(epsp, "epsp");
+    }        
 }
+
+void nu_test(bool just_print = false) {
+    double Tmax = 100;
+    double dt = 0.1;
+    vec t = linspace<vec>(0.0, Tmax, (int)Tmax/dt);
+
+    double fi = datum::inf;
+    mat nupot(t.n_elem, 2);
+    for(size_t ti=0; ti<t.n_elem; ti++) {
+        if(t[ti] >= 15) {
+            fi = 15; 
+        }
+        nupot(ti, 0) = t[ti];
+        nupot(ti, 1) = srm::nu(t[ti], fi);
+    }
+    if(just_print) {
+        nupot.print();
+    } else {        
+        send_arma_mat(nupot, "nupot");
+    }        
+}
+
+
+void srm_test() {
+    srm::SrmNeuron n;
+    for(size_t i=0; i<10; i++) {
+        srm::SrmNeuron* inp_n = new srm::SrmNeuron();
+        n.add_input(inp_n, 10);
+    }
+    n.in[5]->y << 15 << 16 << 17 << endr;
+    n.in[6]->y << 15.5 << 16.5 << 17.5 << endr;
+
+    double Tmax = 100;
+    double dt = 0.1;
+    vec t = linspace<vec>(0.0, Tmax, (int)Tmax/dt);
+    mat pot(t.n_elem, 2);
+    for(size_t ti=0; ti<t.n_elem; ti++) {
+        pot(ti, 0) = t[ti];
+        pot(ti, 1) = n.u(t[ti]);     
+    }
+    pot.print();
+}
+
+PROGRAM_INFO("SIM TEST", "sim tests"); 
+PARAM_STRING("test", "name for test. default \"all\"", "t", "all");
+PARAM_FLAG("nosend", "if it true. no sending to R", "n");
 
 int main(int argc, char** argv) {
     CLI::ParseCommandLine(argc, argv);
-    std::string test_name("all");
-	if(argc>1) {
-        test_name = std::string(argv[1]);
-        Log::Info << "Tests name was found: " << test_name << "\n";
-    }   
+	const std::string test_name = CLI::GetParam<std::string>("test");   
+    bool nosend = CLI::HasParam("nosend");
     Log::Info << "Tests started" << std::endl;
     if((test_name == "all") || (test_name == "epsp")) {
         Log::Info << "epsp test: " << std::endl;
         Log::Info << "===============================================================" << std::endl;
-        epsp_test();
+        epsp_test(nosend);
+        Log::Info << "===============================================================" << std::endl;
+    }        
+    if((test_name == "all") || (test_name == "nu")) {
+        Log::Info << "nu test: " << std::endl;
+        Log::Info << "===============================================================" << std::endl;
+        nu_test(nosend);
+        Log::Info << "===============================================================" << std::endl;
+    }        
+    if((test_name == "all") || (test_name == "srm")) {
+        Log::Info << "srm test: " << std::endl;
+        Log::Info << "===============================================================" << std::endl;
+        srm_test();
         Log::Info << "===============================================================" << std::endl;
     }        
     return 0;
