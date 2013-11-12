80bbbc2a83c530fae4d748678fd698263d90e91d
alexeyche <alexey.chernushev@gmail.com>
2013-11-13 00:56:26 +0400

    cns

--- cns/cpp/src/sim/srm/srm_main.cpp
+++ cns/cpp/src/sim/srm/srm_main.cpp
@@ -21,36 +21,31 @@ int main(int argc, char** argv)
     std::srand(seed);
     Sim s;
     SrmNeuron* n = new SrmNeuron();
-    double w_start = 10;
-//    n->add_input(new PoissonNeuron(100/sec), w_start);
-//    n->add_input(new PoissonNeuron(100/sec), w_start);
-//    n->add_input(new PoissonNeuron(100/sec), w_start);
-//    n->add_input(new PoissonNeuron(10/sec), w_start);
-//    n->add_input(new PoissonNeuron(10/sec), w_start);
-//    n->add_input(new PoissonNeuron(10/sec), w_start);
-    TimeSeriesGroup g(100, 0*ms, 100); 
-    g.loadPatternFromFile("/var/tmp/d1.csv", 100*ms, 100);
-    send_arma_mat(g.patterns[0].pattern, "pattern");
+    double w_start = 2;
+    n->add_input(new DetermenisticNeuron("3 4 5"), w_start);
+    n->add_input(new DetermenisticNeuron("4 5 6"), w_start);
+    n->add_input(new DetermenisticNeuron("5 6 7"), w_start);
+    n->add_input(new DetermenisticNeuron("6 7 8"), w_start);
+    n->add_input(new DetermenisticNeuron("7 8 9"), w_start);
+    n->add_input(new DetermenisticNeuron("8 9 10"), w_start);
+//    TimeSeriesGroup g(100, 0*ms, 100); 
+//    g.loadPatternFromFile("/var/tmp/d1.csv", 100*ms, 100);
+//    send_arma_mat(g.patterns[0].pattern, "pattern");
 //    send_arma_mat(g.patterns[0].pattern, "d1_stat");
 //    g.loadPatternFromFile("/var/tmp/d2.csv", 100*ms, 0.5);
 //    send_arma_mat(g.patterns[1].pattern, "d2_stat");
-    srm::connectFeedForward(&g, n, 0.2);
-    s.addNeuronGroup(&g);
+//    srm::connectFeedForward(&g, n, 0.2);
+//    s.addNeuronGroup(&g);
     s.addNeuron(n);
 
     s.addStatListener(n, TStatListener::Spike);
     s.addStatListener(n, TStatListener::Prob);
-    s.run(0.2*sec);
-    Log::Info << "Sim finished\n";
-
-    vec t = linspace<vec>(0, 0.2*sec, (int)(0.2*sec)/0.1);
-    vec pp(t.n_elem, 2, fill::zeros);
-    for(double ti=0; ti<t.n_elem; ti++) {
-        pp(ti,0) = t(ti);
-        pp(ti,1) = n->p(t(ti));
-    }
-    send_arma_mat(pp, "pp");
-    survFunction(0.2*sec, n);
+    vec pp(100);
+    for(size_t num=0; num<100; num++) {        
+        double t_run = 0.05*sec;
+        s.run(t_run);
+        pp(num) = survFunction(t_run, n);
+    }        
 //    for(size_t ni=0; ni<s.stoch_elem.size(); ni++) {
 //        Log::Info << "id: " << s.stoch_elem[ni]->id() << "\n";
 //        s.stoch_elem[ni]->y.print();
