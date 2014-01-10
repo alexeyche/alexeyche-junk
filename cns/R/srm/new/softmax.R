4eea4a52c3704caa0cfd289896b67e5d36ceac7c
alexeyche <alexey.chernushev@gmail.com>
2014-01-10 18:28:03 +0400

    cns

--- cns/R/srm/new/softmax.R
+++ cns/R/srm/new/softmax.R
@@ -21,13 +21,34 @@ SMNeuronLayer <- setRefClass("SMNeuronLayer", fields = list(w = "list", id_conns
                                               }))
 
 
-nl = SMNeuronLayer(w = list(c(3,3,3,3,3), c(3,3,3,3,3)), 
-              id_conns = list(c(1,2,3,4,5), c(1,2,3,4,5)), 
-              ids = c(6,7))
+nl = SMNeuronLayer(w = list(c(3,3,3,3,3), c(3,3,3,3,3)),
+                   id_conns = list(c(1,2,3,4,5), c(1,2,3,4,5)), 
+                   ids = c(6,7))
 
 r = sapply(seq(0, 20, by=0.5), function(t) nl$r(t, net))
 
-# exp( sum( epsp(w1, x_i) ) )
-# ---------------------------------------------------------
-# exp( sum( epsp(w1, x_i) ) ) + exp( sum( epsp(w2, x_i) ) )
+#              exp(  wk %*% grap_epsp(t, x) )
+# rk(t, x) = ---------------------------------
+#            sum exp( wl %*% grap_epsp(t, x) )
+#             l
 
+# log Pk(y = [tfi] | x ) = sum log( rk(tfi, x) ) -   int rk(t, x) dt  
+#                           i                      [0,T] 
+
+
+# dlog Pk                           drk            1               drk
+# ------ (y = [tfi] | x ) =    sum  -- (tfi, x)  -----    -   int  ---(t, x) dt 
+# dwk                      i=[1,n]  dwk         rk(tfi, x)  [0,T]  dwk
+
+
+# drk
+# ---(t) = ( 1 - rk(t) ) grab_epsp(t, x)
+# dwk
+#
+# E = SUM(i=1,N){ SUM(k=1,O){ (yk-tk)^2 } } 
+# 
+# yk = exp(wk*zk)/SUM(j=1,O){ exp(wj*zj) } 
+# 
+# drk/dwk = zk*rk - zk*yk^2 = zk*yk*(1-yk) % Looks familiar 
+# 
+# dyk/dwi = -zi*yk^2 * exp(wi*zi-wk*zk) % Don't recall seeing this before 
