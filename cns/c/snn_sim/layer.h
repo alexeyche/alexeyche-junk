44820f77ad9673c175c054b8d1a368b22376564b
alexeyche <alexey.chernushev@gmail.com>
2014-06-16 19:05:32 +0400

    cns

--- cns/c/snn_sim/layer.h
+++ cns/c/snn_sim/layer.h
@@ -17,52 +17,8 @@
 #include <neuron_funcs.h>
 #include <util/io.h>
 #include <learn.h>
-#include <learn/optim.h>
-#include <learn/res_stdp.h>
 
-#define SYN_ACT_TOL 0.0001 // value of synapse needed to delete 
-
-#define BACKPROP_POT 1
-#define SFA 0
-#define REFR 0
-#define FS_INH 0
-
-typedef enum {EXC, INH} nspec_t;
-
-struct learn_t;
-
-struct SRMLayer {
-    size_t id;
-    //consts
-
-    size_t N;
-    size_t *ids;
-    nspec_t *nt;
-
-    // structure
-    double **W;
-    size_t **id_conns;
-    int *nconn;
-    
-    // vars 
-    double **syn;
-    double **syn_spec;
-    double *a;
-
-    struct learn_t *ls_t;
-    
-    indLList **active_syn_ids;
-    unsigned char *fired;
-    unsigned char **syn_fired;
-    double *pacc;
-    double *axon_del;
-    double **syn_del;
-    double *gr;
-    double *ga;
-    double *gb;
-    
-    double *p_set;
-    //stat
+struct LayerStat {
     unsigned char statLevel;
     doubleVector **stat_u;
     doubleVector **stat_p;
@@ -72,37 +28,53 @@ struct SRMLayer {
 };
 
 
-typedef struct SRMLayer SRMLayer;
-typedef SRMLayer* pSRMLayer;
+struct Layer {
+    size_t id;
+    //consts
+    size_t N;
+    size_t *ids;
+    nspec_t *nt;
+    
+    // structure
+    double **W;
+    size_t **id_conns;
+    int *nconn;
+    
+    // vars 
+    double *u;
+    double **syn;
+    double **syn_spec;
+    
+    // learn structure 
+    struct learn_t *ls_t;
 
-#include <util/templates_clean.h>
-#define T pSRMLayer
-#define DESTRUCT deleteSRMLayer
-#include <util/util_vector_tmpl.h>
+    // runtime stuff
+    indLList **active_syn_ids;
+    unsigned char *fired;
+    unsigned char **syn_fired;
 
-typedef struct SynSpike {
-    double t;
-    size_t n_id;
-    size_t syn_id;
-} SynSpike;
+    // delays
+    double *axon_del;
+    double **syn_del;
 
+    // stat
+    LayerStat *stat;
 
+    // methods
+    void (*calculateMembranePotentials)(Layer *l, const size_t *ni, const Constants *s, const double *t);
+    void (*calculateSpike_default)(Layer *l, const size_t *ni);
+};
 
-SRMLayer* createSRMLayer(size_t N, size_t *glob_idx, unsigned char statLevel);
-void printSRMLayer(SRMLayer *l);
-void deleteSRMLayer(SRMLayer *l);
-void configureSRMLayer(SRMLayer *l, const indVector *inputIDs, const indVector *outputIDs, Constants *c);
+typedef struct Layer Layer;
 
-void simulateSRMLayerNeuron(SRMLayer *l, const size_t *id_to_sim, const struct Sim *s, const double *t);
-pMatrixVector* serializeSRMLayer(SRMLayer *l);
-void loadSRMLayer(SRMLayer *l, Constants *c, pMatrixVector *data);
-void toStartValuesSRMLayer(SRMLayer *l, Constants *c);
-void propagateSpikeSRMLayer(SRMLayer *l, const size_t *ni, const SynSpike *sp, const Constants *c);
-void resetSRMLayerNeuron(SRMLayer *l, const size_t *ni);
-double getSynDelay(SRMLayer *l, const size_t *id, const size_t *syn_id);
-void setSynapseSpeciality(SRMLayer *l, size_t n_id, size_t syn_id, double spec);
-size_t getLocalNeuronId(SRMLayer *l, const size_t *glob_id);
-const size_t getGlobalId(SRMLayer *l, const size_t *ni);
-double layerConstD(SRMLayer *l, doubleVector *v);
+// common methods
+Layer* createLayer(size_t N, size_t *glob_idx, unsigned char statLevel);
+void deleteLayer(Layer *l);
+void configureLayer(Layer *l, const indVector *inputIDs, const indVector *outputIDs, Constants *c);
+
+// default virtuals
+void calculateMembranePotentials_default(Layer *l, const size_t *ni, const Constants *s, const double *t);
+void calculateSpike_default(Layer *l, const size_t *ni);
+
 
 #endif
