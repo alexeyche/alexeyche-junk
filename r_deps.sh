1ae0cd31f7bb3399263f4056d47019c323c858ff
alexeyche <alexeyche@alexeyche-lat.(none)>
2014-08-11 10:43:23 +0400

    cns

---
+++ r_deps.sh
@@ -0,0 +1,6 @@
+sudo apt-get install gfortran
+sudo apt-get install g++
+sudo apt-get install gcc
+sudo apt-get install libreadline6 libreadline6-dev
+sudo apt-get install libx11-dev
+sudo apt-get install libxt-dev
