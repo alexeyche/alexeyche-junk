15ee416fa5c04c918314f08a6d144f112bf17cba
alexeyche <alexey.chernushev@gmail.com>
2014-08-15 18:48:46 +0400

    cns

--- cns/rust/layers/layer.rs
+++ cns/rust/layers/layer.rs
@@ -1,30 +1,67 @@
+use std::rand;
+use std::rand::distributions::{IndependentSample, Range};
+
+pub trait NetNeuron {
+    fn new(id: uint) -> Self;
+    fn add_connection(&mut self, id : uint, w : f32) -> ();
+    fn getId(&self) -> uint;
+}
 
 pub trait NetLayer {
     fn new(size: uint, id : uint, glob_id: &mut uint) -> Self;
+//    fn connect_feed_forward(&mut self, l: &mut NetLayer, conn_prob: f32) -> ();
+    fn getNeurons(&mut self) -> &mut Vec<Box<NetNeuron>>;
+    
+    fn connect_feed_forward(&mut self, l: &mut NetLayer, conn_prob: f32) {
+        let runif = Range::new(0f32, 1f32);
+        let mut rng = rand::task_rng();
+        for pre in self.getNeurons().iter() {
+            for post in l.getNeurons().mut_iter() {
+                let coin = runif.ind_sample(&mut rng);
+                if coin >= conn_prob {
+                    println!("{} >= {}",  coin, conn_prob);
+                    post.add_connection(pre.getId(), 0.0);
+                }
+            }
+        }            
+    }
+
 }
 
 #[deriving(Show)]
-struct InputNeuron {
+pub struct InputNeuron {
     id : uint,
+}
+
+impl NetNeuron for InputNeuron {
+    fn new(id: uint) -> InputNeuron {
+        InputNeuron { id : id }
+    }
+    fn add_connection(&mut self, id : uint, w : f32) -> () {
+        println!("InputLayer is not supposed to get connections");
+    }
+    fn getId(&self) -> uint {
+        return self.id;
+    }
 }
 
 pub struct InputLayer {
     id : uint,
-    neurons: Vec<InputNeuron>,
+    neurons: Vec<Box<NetNeuron>>,
 }
-impl InputNeuron {
-    fn new(id: uint) -> InputNeuron {
-        InputNeuron { id : id }
-    }
-}
-impl NetLayer for InputLayer{
+
+
+impl NetLayer for InputLayer {
     fn new(size: uint, id : uint, glob_id: &mut uint) -> InputLayer {
        let mut l = InputLayer { neurons : vec!{}, id : id };
        for i in range(0u, size) {
-           l.neurons.push( InputNeuron::new(*glob_id) );
+//           l.neurons.push( InputNeuron::new(*glob_id) );
            *glob_id+=1;
        }
        return l;
+    }
+    fn getNeurons(&mut self) -> &mut Vec<Box<NetNeuron>> {
+        return &mut self.neurons;
     }
 }
 
@@ -38,31 +75,41 @@ struct Neuron {
     w : Vec<uint>,
 }
 
-impl Neuron {
+impl NetNeuron for Neuron {
     fn new(id: uint) -> Neuron {
         Neuron { x : vec!{}, y : 0.0, id : id, id_conns : vec!{}, w : vec!{} }
     }
+    fn add_connection(&mut self, id : uint, w : f32) -> () {
+        println!("===")
+    }
+    fn getId(&self) -> uint {
+        return self.id;
+    }
+
 }
 
-#[deriving(Show)]
 pub struct Layer {
     id : uint, 
-    neurons : Vec<Neuron>,
+    neurons : Vec<Box<NetNeuron>>,
 }
 
 impl NetLayer for Layer{
     fn new(size: uint, id : uint, glob_id: &mut uint) -> Layer {
        let mut l = Layer { neurons : vec!{}, id : id };
        for i in range(0u, size) {
-           l.neurons.push( Neuron::new(*glob_id) );
+//           l.neurons.push( box Neuron::new(*glob_id) );
            *glob_id+=1;
        }
        return l;
     }
-//    pub fn connect_feed_forward(&mut self, l: &Layer, in_prob: f32, out_prob: f32) {
-//    }
-//    pub fn connect_reccurrent(&mut self, prob: f32) {
-//    }
+    
+    fn getNeurons(&mut self) -> &mut Vec<Box<NetNeuron>> {
+        return &mut self.neurons;
+    }
 }
 
+impl Layer {
+    pub fn connect_reccurrent(&mut self, prob: f32) {
+    }
+}
 
