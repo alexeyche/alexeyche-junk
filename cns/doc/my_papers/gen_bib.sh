97ab051928744f9576d94fae7e3f6ba057060fba
alexeyche <alexey.chernushev@gmail.com>
2014-08-24 19:03:34 +0400

    cns

--- cns/doc/my_papers/gen_bib.sh
+++ cns/doc/my_papers/gen_bib.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-latex ./review && bibtex ./review && latex ./review && latex ./review
+cp ./review.tex review.tex.bak
+sed -i -e 's|^\\includegraphics|%\\includegraphics|g' ./review.tex
+latex  ./review && bibtex ./review && latex  ./review && latex  ./review
+mv review.tex.bak review.tex
