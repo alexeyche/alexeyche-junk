dfdd20f25d719a36e36a482ee9c9eaed4b0c39fc
alexeyche <alexey.chernushev@gmail.com>
2014-09-16 23:46:51 +0400

    cns

--- cns/R/srm/Rsnn/R/util.R
+++ cns/R/srm/Rsnn/R/util.R
@@ -1,12 +1,5 @@
 
 
-get_unique_ids <- function(n) {
-  if(!exists('ID_MAX')) { assign('ID_MAX', 0, envir=.GlobalEnv) }
-  id_max = get('ID_MAX',.GlobalEnv)
-  assign('ID_MAX', id_max+n, envir=.GlobalEnv)
-  return((id_max+1):(id_max+n))
-}
-
 
 plotl <- function(x) {
     plot(x, type="l")
@@ -55,66 +48,13 @@ blank_net = function(N) {
     return(net)
 }
 
-make_conn_matrix = function(layers) {
-    max_conn_id = -1
-    min_conn_id = Inf
-    for(neurons in layers) {
-        invisible(sapply(neurons$id_conns(), function(cid) { 
-            if(length(cid) !=0) {
-                max_conn_id <<-max(max_conn_id, max(cid))
-                min_conn_id <<-min(min_conn_id, min(cid))
-            }
-        }))
-    }
-    conn_m = matrix(0, nrow=max_conn_id, ncol=sum(sapply(layers, function(n) n$len()))) 
-    return(conn_m)
-}
 
-
-get_stat_matrix <- function(neurons, stat_list) {
-    stat_m = make_conn_matrix(list(neurons))      
-    for(ni in 1:neurons$len()) {
-        for(syn_num in 1:length(neurons$id_conns()[[ni]])) {
-            stat_m[ neurons$id_conns()[[ni]][syn_num] , ni] = stat_list[[ni]][syn_num]
-        }
-    }
-    return(stat_m)
-}
 
 gr_pl = function(m) {
   levelplot(m, col.regions=colorRampPalette(c("black", "white")))
 }
 
-saveModelToFile = function(n, model_file) {
-    l = list()
-    l[[1]] = get_stat_matrix(n, n$obj$W)
-    l[[2]] = get_stat_matrix(n, n$obj$syn_spec)
-    l[[3]] = get_stat_matrix(n, n$obj$syn_del)
-    l[[4]] = n$obj$axon_del
-    saveMatrixList(model_file, l)
-}
 
-loadModelFromFile = function(n, model_file) {
-  if(file.exists(paste(model_file, ".idx", sep=""))) {  
-    W = loadMatrix(model_file, 1)
-    syn_spec = loadMatrix(model_file, 2)
-    syn_del = loadMatrix(model_file, 3)
-    axon_del = loadMatrix(model_file, 4)
-    
-    n$obj$axon_del = axon_del
-    invisible(sapply(1:N, function(id) { 
-      id_to_conn = which(W[,id] != 0)
-      n$obj$W[[id]] <<- W[id_to_conn, id] 
-      n$obj$id_conns[[id]] <<- id_to_conn
-      n$obj$syn_spec[[id]] <<- syn_spec[id_to_conn, id]
-      n$obj$syn_del[[id]] <<- syn_del[id_to_conn, id]
-    }))  
-    cat("Load - Ok\n")
-  } else {
-    cat("Can't find file for model ", model_file, "\n")
-  }
-  
-}
 
 getSpikesFromMatrix = function(sp) {
     net = blank_net(nrow(sp))
--- cns/R/srm/build.sh
+++ cns/R/srm/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-packagename=snn
+packagename=Rsnn
 ver=1.0
 
 set -x
 pushd $packagename
 Rscript -e 'Rcpp::compileAttributes()'
 popd
+SNN_INC="/home/alexeyche/prog/alexeyche-junk/cns/c/snn_sim"
+SNN_LIB="/home/alexeyche/prog/alexeyche-junk/cns/c/snn_sim"
 R CMD build $packagename
-R CMD INSTALL --build ${packagename}_${ver}.tar.gz
+R CMD INSTALL --build ${packagename}_${ver}.tar.gz --configure-args="--with-snn-include=$SNN_INC --with-snn-lib=$SNN_LIB"
