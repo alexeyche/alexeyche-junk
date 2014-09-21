ce1cac4929f2d31b61db113d1569ae63eb352c26
alexeyche <alexey.chernushev@gmail.com>
2014-09-21 15:00:07 +0400

    cns

--- cns/R/srm/Rsnn_scripts/test.R
+++ cns/R/srm/Rsnn_scripts/test.R
@@ -6,7 +6,7 @@ require(Rsnn)
 M = 100
 const_ini = "/home/alexeyche/prog/alexeyche-junk/cns/c/snn_sim/constants.ini"
 N = 100
-jobs = 4
+jobs = 8
 statLevel = 0
 
 const = RConstants$new(const_ini)
@@ -15,9 +15,9 @@ const$setValue("layer","N", N)
 const$setValue("layer","neuron_type", "WtaLayer")
 const$setValue("layer","learning_rule", "SimpleSTDP")
 const$setValue("layer","prob_fun", "Exp")
-const$setValue("layer","lrate", 0.04)
+const$setValue("layer","lrate", 0.001)
 const$setValue("layer","weight_decay_factor", 0.015)
-
+const$setValue("layer","net_edge_prob", 1.0)
 s = RSim$new(const, statLevel, jobs)
 
 spikes = getSpikesFromMatrix(loadMatrix("/home/alexeyche/prog/sim/spikes/ucr/1_train_spikes",1))
@@ -25,7 +25,7 @@ spikes = getSpikesFromMatrix(loadMatrix("/home/alexeyche/prog/sim/spikes/ucr/1_t
 # spikes=blank_net(M)
 # spikes[[1]] = c(10,20)
 # spikes[[2]] = c(20,30)
-max_ep = 100
+max_ep = 
 for(i in 1:max_ep) {
     s$setInputSpikes(spikes)
     out_sp = s$run()
@@ -36,4 +36,4 @@ stat = s$getLayerStat(0)
 
 prast(out_sp,T0=0,Tmax=1000)
 
-gr_pl(t(s$W()[[1]]))
+gr_pl(s$W()[[1]])
