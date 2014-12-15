1d132238905a163299667aa73a74beb6217e97e5
alexeyche <alexey.chernushev@gmail.com>
2014-12-15 20:14:27 +0400

    cns

--- cns/cpp/r_package/r_scripts/gen_test_data.R
+++ cns/cpp/r_package/r_scripts/gen_test_data.R
@@ -1,11 +1,20 @@
 
+
+require(Rsnn)
+
 N = 100
 
-mean_rate = 15 # Hz
+mean_rate = 10 # Hz
 
 correllated_neurons = 5
 correlation = 0.3
 
-len = 1000
+len = 1000 # ms
 
-lx = rpois(len, mean_rate/1000.0) # ms precision
+net = vector("list",N)
+for(ni in 1:N) {
+    spikes_bin = rpois(len, mean_rate/1000.0)  # ms precision
+    net[[ni]] = which(spikes_bin == 1)
+}
+
+prast(net)
