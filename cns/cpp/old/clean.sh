065117f50ed48193bfca6c05529e35e0bbd6cbb6
alexeyche <alexey.chernushev@gmail.com>
2015-02-01 21:36:10 +0400

    cns

--- cns/cpp/old/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pushd `dirname $0`
-set -x
-rm -rf ./build/*
-popd
--- cns/cpp/old/test_scripts/generate_baseline_spikes.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -x
-sudo rngd -r /dev/urandom -o /dev/random
-dd if=/dev/random of=/var/tmp/random_spikes count=8192
