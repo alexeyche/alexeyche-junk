910b38d83bfca88c0a0e5dbecc68145c73e009d1
alexeyche <alexey.chernushev@gmail.com>
2015-02-09 01:46:07 +0400

    cns

--- cns/cpp/sources/dnn/neuron.h
+++ cns/cpp/sources/dnn/neuron.h
@@ -5,22 +5,23 @@
 namespace dnn {
 
 template <typename State, typename Eval>
-class NeuronInput : public DynamicObject<State, Eval> {
-public:
-
-};
+using NeuronInput = DynamicObject<State, Eval>;
 
 
-template <typename State, typename Eval = double, typename InputEval = double, typename SynapseSystemEval = double>
+template <typename State, 
+		  typename Eval = bool, 
+		  typename InputEval = double,
+		  typename SynapseSystemEval = double, 
+		  typename ActFunctionEval = bool>
 class SpikeNeuron : public DynamicObject<State, Eval> {
 public:
     typedef EvalObject<InputEval> InputType;
     typedef EvalObject<SynapseSystemEval> SynapsesType;
+    typedef EvalObject<ActFunctionEval> ActFunctionType;
 
-    SpikeNeuron(InputType &i, SynapsesType &syn) : input(i), synapses(syn) {}
+    SpikeNeuron(InputType &i, SynapsesType &syn, ActFunctionType &af) : input(i), synapses(syn), act_f(af) {}
     
-    virtual void spiked() = 0;
-
+    ActFunctionType &act_f;
     InputType &input;
     SynapsesType &synapses;
 };
@@ -31,14 +32,8 @@ public:
     typedef EvalObject<InputEval> NeuronType;
 
     SpikeActFunction(NeuronType &_n) : neuron(_n) {}
-    virtual bool eval_spike() = 0;
-    
-    bool eval() {
-    	bool spiked = eval_spike();
-    	if(spiked) neuron.eval_spike();
-    	return spiked;
-    }
 
+    
     NeuronType &neuron;
 };
 
