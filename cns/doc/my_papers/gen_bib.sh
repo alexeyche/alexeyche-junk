3cd5c7be99997eaf0bb9643ca9825eafcd708dee
alexeyche <alexey.chernushev@gmail.com>
2015-10-10 12:59:19 +0300

    cns

--- cns/doc/my_papers/gen_bib.sh
+++ cns/doc/my_papers/gen_bib.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-cp review.tex review.tex.bak
-sed -i -e 's|^\\includegraphics|%\\includegraphics|g' ./review.tex
-latex  ./review && bibtex ./review && latex  ./review && latex  ./review
-mv review.tex.bak review.tex
+set -x
+[ -z $1 ] && exit 1
+[ -f $1 ] || exit 1
+cp $1 ${1}.bak
+bn=${1%%.tex} 
+sed -i -e 's|^\\includegraphics|%\\includegraphics|g' $1
+latex  ./$bn && bibtex $bn && latex $bn && latex $bn
+mv ${1}.bak review.tex
