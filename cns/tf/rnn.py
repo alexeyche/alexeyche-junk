b53d02ada1d1cb0bb47421568718947669b5500e
alexeyche <alexey.chernushev@gmail.com>
2016-07-13 11:59:07 +0300

    junk

--- cns/tf/rnn.py
+++ cns/tf/rnn.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops.rnn_cell import RNNCell
+from tensorflow.python.ops.rnn_cell import MultiRNNCell
 from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import rnn
@@ -105,93 +106,102 @@ sigma = 0.5
 dt = 0.1
 
 
-signal_form = get_form(dt, sigma)
+#signal_form = get_form(dt, sigma)
 
 input_size = 10
 batch_size = 1
-net_size = 1
+net_size = 10
 epochs = 400
 seq_size = 10
 lrate = 0.1
 decay_rate=1
 
-thetaNeuron = ThetaRNNCell(net_size, dt, sigma)
+#thetaNeuron = ThetaRNNCell(net_size, dt, sigma)
+stacked = MultiRNNCell([ThetaRNNCell(net_size, dt, sigma) for _ in xrange(2) ])
+
 inputs  = [ tf.placeholder(tf.float32, shape=(batch_size, input_size), name="Input_{}".format(idx)) for idx in xrange(seq_size) ]
 
-init_state = tf.placeholder(tf.float32, shape=(batch_size, net_size), name="State")
+#init_state = state = stacked.zero_state(batch_size, tf.float32) 
+state = tf.placeholder(tf.float32, shape=(batch_size, net_size), name="State")
+#outputs, state = rnn.rnn(thetaNeuron, inputs, init_state)
 
-outputs, state = rnn.rnn(thetaNeuron, inputs, init_state)
 
+# sess = tf.Session()
+# writer = tf.train.SummaryWriter("{}/tf".format(os.environ["HOME"]), sess.graph)
 
-sess = tf.Session()
-writer = tf.train.SummaryWriter("{}/tf".format(os.environ["HOME"]), sess.graph)
+# data = [ np.zeros((batch_size, input_size)) for _ in xrange(seq_size) ]
 
-data = [ np.zeros((batch_size, input_size)) for _ in xrange(seq_size) ]
+# for seq_i in xrange(seq_size):
+#    ni = seq_i % input_size
+#    data[seq_i][0, ni] = 1.0
+#    # data[seq_i][0, :] = np.convolve(signal_form, data[seq_i][0, :], mode="same")
 
-for seq_i in xrange(seq_size):
-   ni = seq_i % input_size
-   data[seq_i][0, ni] = 1.0
-   # data[seq_i][0, :] = np.convolve(signal_form, data[seq_i][0, :], mode="same")
+# #data[0] = 1.0 * np.ones((batch_size, input_size))
 
-#data[0] = 1.0 * np.ones((batch_size, input_size))
+# #target_seq = gen_poisson(np.asarray([4.0]*net_size), seq_size, 0.01)
 
-#target_seq = gen_poisson(np.asarray([4.0]*net_size), seq_size, 0.01)
+# # for ni in xrange(net_size):
+# #   target_seq[:, ni] = np.convolve(signal_form, target_seq[:, ni], mode="same")
 
-# for ni in xrange(net_size):
-#   target_seq[:, ni] = np.convolve(signal_form, target_seq[:, ni], mode="same")
+# target_seq = np.zeros((seq_size, net_size))
+# target_seq[5,:] = 1.0
+# target_data = [ np.asarray([ target_seq[si, :] for _ in xrange(batch_size) ]) for si in xrange(seq_size) ]
 
-target_seq = np.zeros((seq_size, net_size))
-target_seq[5,:] = 1.0
-target_data = [ np.asarray([ target_seq[si, :] for _ in xrange(batch_size) ]) for si in xrange(seq_size) ]
+#targets = [ tf.placeholder(tf.float32, shape=(batch_size, net_size), name="Target") for si in xrange(seq_size) ]
+#loss = tf.add_n([ tf.nn.l2_loss(target - outputs) for output, target in zip(outputs, targets) ]) / seq_size / batch_size / net_size
 
-targets = [ tf.placeholder(tf.float32, shape=(batch_size, net_size), name="Target") for si in xrange(seq_size) ]
-loss = tf.add_n([ tf.nn.l2_loss(target - outputs) for output, target in zip(outputs, targets) ]) / seq_size / batch_size / net_size
-
-lr = tf.Variable(0.0, trainable=False)
-tvars = tf.trainable_variables()
-grads_raw = tf.gradients(loss, tvars)
+#lr = tf.Variable(0.0, trainable=False)
+#tvars = tf.trainable_variables()
+#grads_raw = tf.gradients(loss, tvars)
 # grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars), 5.0)
 
-optimizer = tf.train.GradientDescentOptimizer(lr)
-train_step = optimizer.apply_gradients(zip(grads_raw, tvars))
+#optimizer = tf.train.GradientDescentOptimizer(lr)
+#train_step = optimizer.apply_gradients(zip(grads_raw, tvars))
 
-init = tf.initialize_all_variables()
+#init = tf.initialize_all_variables()
 
-weights, recc_weights = [], []
-with tf.device("/cpu:0"):
-    sess.run(init)
-    for e in xrange(epochs):
-        elrate = lrate * (decay_rate ** e)
-        print "Epoch {}, learning rate {}".format(e, elrate),
-        sess.run(tf.assign(lr, elrate))
 
-        feed_dict = {k: v for k, v in zip(inputs, data)}
-        feed_dict[init_state] = np.zeros((batch_size, net_size))
-        feed_dict.update({k: v for k, v in zip(targets, target_data)})
+for i in xrange(seq_size):
+    output, state = stacked(inputs[i], state)
 
-        fetch = [outputs, state]
-        fetch += [thetaNeuron.W]
-        fetch += [thetaNeuron.U]
-        fetch += [loss, train_step]
-        fetch += grads_raw[-3:]
 
-        out = sess.run(fetch, feed_dict)
-        outputs_v, state_v = np.asarray(out[0]), out[1]
-        weights.append(out[2])
-        recc_weights.append(out[3])
+final_state = state
 
-        loss_v, train_step_v = out[4], out[5]
-        gW, gU, gB = out[6], out[7], out[8]
-        print ", train loss {}".format(loss_v)
-        if e % 50 == 0 or e == epochs-1:
-            plt.plot(outputs_v[:,0,0]); plt.show()
-            # out_one = outputs_v[:,0,0]
-            # out = outputs_v[:,0, :]
+# weights, recc_weights = [], []
+# with tf.device("/cpu:0"):
+#     sess.run(init)
+#     for e in xrange(epochs):
+#         elrate = lrate * (decay_rate ** e)
+#         print "Epoch {}, learning rate {}".format(e, elrate),
+#         sess.run(tf.assign(lr, elrate))
 
-            # plt.figure(1)
-            # plt.subplot(2,1,1)
-            # plt.imshow(target_seq.T)
-            # plt.subplot(2,1,2)
-            # plt.imshow(out.T)
-            # plt.show()
+#         feed_dict = {k: v for k, v in zip(inputs, data)}
+#         feed_dict[init_state] = np.zeros((batch_size, net_size))
+#         feed_dict.update({k: v for k, v in zip(targets, target_data)})
+
+#         fetch = [outputs, state]
+#         fetch += [thetaNeuron.W]
+#         fetch += [thetaNeuron.U]
+#         fetch += [loss, train_step]
+#         fetch += grads_raw[-3:]
+
+#         out = sess.run(fetch, feed_dict)
+#         outputs_v, state_v = np.asarray(out[0]), out[1]
+#         weights.append(out[2])
+#         recc_weights.append(out[3])
+
+#         loss_v, train_step_v = out[4], out[5]
+#         gW, gU, gB = out[6], out[7], out[8]
+#         print ", train loss {}".format(loss_v)
+#         if e % 50 == 0 or e == epochs-1:
+#             plt.plot(outputs_v[:,0,0]); plt.show()
+#             # out_one = outputs_v[:,0,0]
+#             # out = outputs_v[:,0, :]
+
+#             # plt.figure(1)
+#             # plt.subplot(2,1,1)
+#             # plt.imshow(target_seq.T)
+#             # plt.subplot(2,1,2)
+#             # plt.imshow(out.T)
+#             # plt.show()
 
