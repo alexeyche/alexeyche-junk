f2c139f5321a95ab0c3b318a9805c62302b34b78
alexeyche <alexey.chernushev@gmail.com>
2016-12-05 07:56:55 +0300

    junk

--- cns/seq_learn/test.py
+++ cns/seq_learn/test.py
@@ -9,42 +9,66 @@ def rep_constant(shape, value):
     r.fill(value)
     return r
 
+def safe_ln(x, minval=0.0000000001):
+    # return np.log(x)
+    return np.log(x.clip(min=minval))
 
 class Stats(object):
     def __init__(self, epochs, Tsize, pop_size):
         self.p = np.zeros((Tsize, pop_size))
         self.u = np.zeros((Tsize, pop_size))
         self.ll = np.zeros((Tsize, pop_size))
-        self.W = np.zeros((Tsize, pop_size, pop_size))
         self.epsp = np.zeros((Tsize, pop_size))
+        self.delta_r = np.zeros((Tsize,))
+        self.r = np.zeros((Tsize,))
+        self.mean_r = np.zeros((Tsize,))
+        
+class State(object):
+    def __init__(self, pop_size):
+        self.r = 0.0
+        self.mean_r = 0.0
+        self.spikes_smoothed = np.zeros((pop_size,))
+        self.trace = np.zeros((pop_size,pop_size))
+        self.dW = np.zeros((pop_size,pop_size))
 
-
-# np.random.seed(25)
+np.random.seed(25)
 
 v_size = 100
-h_size = 0
+h_size = 100
 pop_size = v_size + h_size
 u_rest = -5.0
 beta = 1.0
 tau_decay = 10.0
 tau_rise = 2.0
 tau_refr = 5.0
-tau_look_around = 5*tau_decay
-lrate = 100.0
-T = 100
+tau_look_around = 20*tau_decay
+
+
+# tau_trace = 10.0
+# tau_mean_trace = 100.0
+
+
+T = 200.0
 dt = 1.0
 dt_sec = dt/1000.0
 Tsize = int(T/dt)
 weight_factor = 0.0
 W = - weight_factor +  2.0 * weight_factor*np.random.rand(pop_size, pop_size)
-epochs = 50
+
+lambda_first = 1.0/50.0 #1.0/T
+lambda_second = 1.0/100.0 #lambda_first/10.0
+
+lrate = 1.0
+epochs = 100
 
 act = lambda u: 1.0/(1.0 + np.exp(-beta * u))
 epsp = lambda x: (1.0/(tau_decay - tau_rise)) * (np.exp(-x/tau_decay) - np.exp(-x/tau_rise))
 post_spike_kernel = lambda x: -100.0 * np.exp(-x/tau_refr)
 act_factor = lambda _: beta
 
-def step(ti, t, spikes, stats, simulate_visible=False, learn=True):
+def step(ti, t, spikes, state, stats, simulate_visible=False, learn=True):
+    spikes_smoothed = np.zeros((pop_size,))
+
     past_idx = max(0, int(ti-dt*tau_look_around))
 
     spikes_past = spikes[past_idx:ti, :].todense()
@@ -55,11 +79,14 @@ def step(ti, t, spikes, stats, simulate_visible=False, learn=True):
 
     for t_sp, n_id in zip(spike_times, neuron_ids):
         t_sp += t - spikes_past.shape[0]*dt
-        x_e = epsp(t - t_sp)
-        u += W[n_id, :] * x_e
+        
+        x_j = epsp(t - t_sp)
 
-        stats.epsp[ti, n_id] = x_e
-
+        spikes_smoothed[n_id] += x_j
+        u += W[n_id, :] * x_j
+        
+        stats.epsp[ti, n_id] = spikes_smoothed[n_id]
+        
         u[n_id] += post_spike_kernel(t - t_sp)
 
     p = act(u)
@@ -73,54 +100,94 @@ def step(ti, t, spikes, stats, simulate_visible=False, learn=True):
 
     x = np.squeeze(np.array(spikes[ti, :].todense()))
 
+    ll = x * safe_ln(p) + (1.0 - x) * safe_ln(1.0 - p)
+
+    if np.any(np.isnan(ll)):
+        raise Exception("NAN: \nll {}\np: {}, W: {}".format(ll, p, W))
+
+    state.mean_r = (1.0 - lambda_second) * state.mean_r + lambda_second * state.r   # need state.r(t-1)
+    state.r = (1.0 - lambda_first) * state.r + lambda_first * np.sum(ll[:v_size])
+
     if learn:
-        for t_sp, n_id in zip(spike_times, neuron_ids):
-            t_sp += t - spikes_past.shape[0]*dt
-            W[n_id, :] += lrate * act_factor(u) * (x - p) * epsp(t - t_sp)
+        factor = state.r - state.mean_r    
+        for _, n_id in zip(spike_times, neuron_ids):
+            # state.trace[n_id, :] = (1.0 - lambda_first) * state.trace[n_id, :] + lambda_first * (
+            #     act_factor(u) * (x - p) * state.spikes_smoothed[n_id]
+            # )
+            # W[n_id, :] += lrate * state.trace[n_id, :] * (1.0 if n_id < v_size else factor)
+            # W[n_id, :] = W[n_id, :].clip(-100.0, 100.0)
+        
+            for i in xrange(pop_size):
+                state.dW[n_id, i] += lrate * act_factor(u[i]) * (x[i] - p[i]) * spikes_smoothed[n_id] * (1.0 if i < v_size else factor)
+            
+            if np.any(np.isinf(W[n_id, :])):
+                raise Exception("INF: p: {}, factor: {}, act_factor: {}, x: {}, epsp: {}".format(
+                    p, factor, act_factor(u), x, spikes_smoothed[n_id])
+                )
+
+
+    # state.mean_r += dt * (state.r - state.mean_r)/tau_mean_trace
+    # state.r += dt * (ll - state.r)/tau_trace
 
     stats.u[ti, :] = u
     stats.p[ti, :] = p
-    stats.ll[ti, :] = x * np.log(p) + (1.0 - x) * np.log(1.0 - p)
+    stats.ll[ti, :] = ll
+    stats.delta_r[ti] = state.r - state.mean_r
+    stats.r[ti] = state.r
+    stats.mean_r[ti] = state.mean_r
+    
+    
 
 
 
+given_spikes = sp.lil_matrix((Tsize, pop_size), dtype=np.float32)
 
-
-spikes = sp.lil_matrix((Tsize, pop_size), dtype=np.float32)
-
-# t_rate = 2.0
+# t_rate = 1.0
 # for tt in xrange(Tsize):
 #     for ni in xrange(v_size):
 #         if np.random.random() <= t_rate/Tsize:
-#             spikes[tt, ni] = 1.0
+#             given_spikes[tt, ni] = 1.0
 
 
 for vi in xrange(v_size):
-    spikes[vi, vi] = 1.0
+    given_spikes[vi, vi] = 1.0
+    given_spikes[Tsize - vi-1, vi] = 1.0
 
 
 
 stats = Stats(epochs, Tsize, pop_size)
 
+r, mean_r = 0.0, 0.0
+
 for ep in xrange(epochs):
-    spikes_run = spikes.copy()
+    state = State(pop_size)
+    state.r, state.mean_r = r, mean_r
+    
+    spikes = given_spikes.copy()
     for ti, t in enumerate(np.linspace(0, T, Tsize)):
-        step(ti, t, spikes_run, stats, simulate_visible=False, learn=True)
+        step(ti, t, spikes, state, stats, simulate_visible=False, learn=True)
 
-
+    W += state.dW
+    r, mean_r = state.r, state.mean_r
     print "Epoch {} finished, likelihood: {}".format(ep, np.mean(stats.ll))
-    stats.W[ep, :, :] = W
 
 
+
+stats_eval = Stats(epochs, Tsize, pop_size)
+state_eval = State(pop_size)
 
 spikes_eval = sp.lil_matrix((Tsize, pop_size), dtype=np.float32)
+spikes_eval[:10,:] = given_spikes[:10,:]
 for ti, t in enumerate(np.linspace(0, T, Tsize)):
-    step(ti, t, spikes_eval, stats, simulate_visible=True, learn=False)
+    step(ti, t, spikes_eval, state_eval, stats_eval, simulate_visible=True, learn=False)
 
 
 plt.figure(1)
-plt.imshow(spikes_eval.todense().T, cmap='gray')
+plt.imshow(given_spikes.todense().T, cmap='gray')
 plt.figure(2)
-plt.imshow(spikes.todense().T, cmap='gray')
+plt.imshow(spikes_eval.todense().T, cmap='gray')
+plt.figure(3)
+plt.imshow(W) 
+plt.colorbar()
 plt.show()
 
