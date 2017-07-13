b57b8934f38d4186c09a514fe5030836ab22f47e
Aleksei Chernyshev <aleksei@spotify.com>
2017-07-13 18:09:46 +0200

    junk

--- biomap/main.py
+++ biomap/main.py
@@ -16,52 +16,98 @@ def get_toy_data(n_features, size=2000, seed=2):
     )
     return x_values, y_values
 
+y = np.random.randn(5, 2)
+x = np.random.randn(5, 10)
 
-dog_init = lambda shape, dtype, partition_info: build_dog(shape[0], shape[1], amp_neg=0.0)
-
-input_size = 10
-layer_size = 10
-layers_num = 10
-batch_size = 2000
-
-sizes = [(input_size, layer_size)] + [(layer_size, layer_size) for lid in xrange(layers_num-1)]
-
-x = tf.placeholder(tf.float32, (None, input_size), name="x")
-
-variables = [
-    tf.get_variable("W{}".format(lid), [size_pre, size_post], 
-        initializer=tf.constant_initializer(value=0.01)
-    )
-    for lid, (size_pre, size_post) in enumerate(sizes)
-]
-
-biases = [
-    tf.get_variable("b{}".format(lid), [size_pre], 
-        initializer=tf.constant_initializer(value=-0.1)
-    )
-    for lid, (size_pre, _) in enumerate(sizes)
-]
+w = np.ones((10, 2))
 
 
-net_signals = [x]
-for v, b in zip(variables, biases):
-    lin = tf.matmul(net_signals[-1], v) + b
-    
-    lin += tf.random_normal((batch_size, v.get_shape()[1].value), mean=0.0, stddev=0.5)
+dw = np.zeros(w.shape)
 
-    out = tf.nn.relu(lin)
-    net_signals.append(out)
+for i in xrange(dw.shape[0]):
+	for j in xrange(dw.shape[1]):
+		dw[i, j] += np.mean(y[:, j] * (x[:, i] - y[:, j] * w[i, j]))
+
+vv = x - np.dot(y, w.T)
+dw2 = np.dot(y.T, vv).T
+
+# dw2 = np.asarray([y[n] * v[:, n] for n in xrange(v.shape[1])]).T
+	
 
 
-init = tf.global_variables_initializer()
+# def calc_corr(net_v, variables):
+# 	dws = []
+# 	for li in xrange(1, net_v.shape[0]):
+# 		li_prev = li - 1
+# 		v = variables[li_prev]
+# 		dw = np.zeros(v.shape)
+		
+		
 
-sess = tf.Session()
-sess.run(init)
+# 		for bi in xrange(net_v.shape[1]):
+# 			for i in xrange(dw.shape[0]):
+# 				for j in xrange(dw.shape[1]):
+# 					y = net_v[li, bi, j]
+# 					x = net_v[li_prev, bi, i], 
+# 					dw[i, j] += y * (x - y * v[i, j])
 
-x_values, y_values = get_toy_data(input_size)
+# 		dws.append(dw - np.mean(dw))
+# 	return dws
 
-net_v = sess.run(net_signals, {x: x_values})
+# dog_init = lambda shape, dtype, partition_info: build_dog(shape[0], shape[1], amp_neg=0.0)
 
-net_v = np.asarray(net_v)
+# input_size = 10
+# layer_size = 10
+# layers_num = 10
+# batch_size = 2000
 
-shm(np.mean(net_v, 1)[1:,])
+# sizes = [(input_size, layer_size)] + [(layer_size, layer_size) for lid in xrange(layers_num-1)]
+
+# x = tf.placeholder(tf.float32, (None, input_size), name="x")
+
+# variables = [
+#     tf.get_variable("W{}".format(lid), [size_pre, size_post], 
+#         initializer=tf.constant_initializer(value=0.01)
+#     )
+#     for lid, (size_pre, size_post) in enumerate(sizes)
+# ]
+
+# biases = [
+#     tf.get_variable("b{}".format(lid), [size_pre], 
+#         initializer=tf.constant_initializer(value=-0.1)
+#     )
+#     for lid, (size_pre, _) in enumerate(sizes)
+# ]
+
+
+# net_signals = [x]
+# for li, (v, b) in enumerate(zip(variables, biases)):
+#     lin = tf.matmul(net_signals[-1], v) + b
+#     v_norm = tf.sqrt(tf.reduce_sum(tf.square(v), 0))
+	
+#     lin = tf.Print(lin, [tf.reduce_mean(v_norm)], message="Layer {}, mean norm ".format(li))
+
+#     lin += tf.random_normal((batch_size, v.get_shape()[1].value)) * (0.02/v_norm)
+
+#     out = tf.nn.relu(lin)
+#     net_signals.append(out)
+
+
+# init = tf.global_variables_initializer()
+
+# sess = tf.Session()
+# sess.run(init)
+
+# x_values, y_values = get_toy_data(input_size)
+
+# for e in xrange(10):
+# 	net_v = sess.run(net_signals, {x: x_values})
+
+# 	net_v = np.asarray(net_v)
+# 	dws = calc_corr(net_v, sess.run(variables))
+
+# 	sess.run(tf.group(*[tf.assign_add(v, dv) for v, dv in zip(variables, dws)]))
+	
+# 	print "Epoch {}".format(e)	
+
+# shm(np.mean(net_v, 1)[1:,])
